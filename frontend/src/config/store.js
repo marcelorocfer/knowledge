99ebf7af7c75f42298e870f582ed706d5e2f7931
marcelorocfer <marceloengecomp@gmail.com>
2020-08-24 15:47:12 -0300

    Criação dos componetes e rotas

---
+++ frontend/src/config/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user: {
+            name: 'Usuário Mock',
+            email: 'mock@infotech.com.br'
+        }
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if (isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+
+            console.log('togglemenu = ' + state.isMenuVisible)
+        }
+    }
+})

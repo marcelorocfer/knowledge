adf1f4be45d4d1a1338f2078e1a39bb64e232a17
marcelorocfer <marceloengecomp@gmail.com>
2020-08-25 15:44:53 -0300

    Finalizando o cadastro de usuários

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,11 +1,86 @@
 <template>
     <div class="user-admin">
-        <b-table hover striped :items="users" :fields="fields"></b-table>
+        <b-form>
+            <input type="hidden" id="user-id" v-model="user.id">
+            <b-row>
+                <b-col md="6" sm="12">
+                    <b-form-group label="Nome:" label-for="user-name">
+                        <b-form-input 
+                            id="user-name"
+                            type="text"
+                            v-model="user.name"
+                            :readonly="mode === 'remove'"
+                            placeholder="Informe o Nome do Usuário..."
+                        />
+                    </b-form-group>
+                </b-col>
+                <b-col md="6" sm="12">
+                    <b-form-group label="E-mail:" label-for="user-email">
+                        <b-form-input 
+                            id="user-email"
+                            type="text"
+                            v-model="user.email"
+                            :readonly="mode === 'remove'"
+                            placeholder="Informe o E-mail do Usuário..."
+                        />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <b-form-checkbox id="user-admin" v-show="mode === 'save'" v-model="user.admin" class="mt-3 mb-3">
+                Administrador?
+            </b-form-checkbox>
+            <b-row v-show="mode === 'save'">
+                <b-col md="6" sm="12">
+                    <b-form-group label="Senha:" label-for="user-password">
+                        <b-form-input 
+                            id="user-password"
+                            type="password"
+                            v-model="user.password"
+                            :readonly="mode === 'remove'"    
+                            placeholder="Informe a Senha do Usuário..."
+                        />
+                    </b-form-group>
+                </b-col>
+                <b-col md="6" sm="12">
+                    <b-form-group label="Confirmação de Senha:" label-for="user-confirm-password">
+                        <b-form-input 
+                            id="user-confirm-password"
+                            type="password"
+                            v-model="user.confirmPassword"   
+                            :readonly="mode === 'remove'"          
+                            placeholder="Confirme a Senha..."
+                        />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col xs="12">
+                    <b-button variant="primary" v-if="mode === 'save'" @click="save">
+                        Salvar
+                    </b-button>
+                    <b-button variant="danger" v-if="mode === 'remove'" @click="remove">
+                        Excluir
+                    </b-button>
+                    <b-button class="ml-2" @click="reset">Cancelar</b-button>
+                </b-col>
+            </b-row>
+        </b-form>
+        <hr>
+        <b-table hover striped :items="users" :fields="fields">
+            <template slot="actions" slot-scope="data">
+                <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-button>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global'
+import { baseApiUrl, showError } from '@/global'
 import axios from 'axios'
 
 export default {
@@ -31,6 +106,34 @@ export default {
             axios.get(url).then(res => {
                 this.users = res.data
             })
+        },
+        reset() {
+            this.mode = 'save'
+            this.user = {}
+            this.loadUsers()
+        },
+        save() {
+            const method = this.user.id ? 'put' : 'post'
+            const id = this.user.id ? `/${this.user.id}` : ''
+            axios[method](`${baseApiUrl}/users${id}`, this.user)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        remove() {
+            const id = this.user.id
+            axios.delete(`${baseApiUrl}/users/${id}`)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        loadUser(user, mode='save') {
+            this.mode = mode
+            this.user = { ...user }
         }
     },
     mounted() {

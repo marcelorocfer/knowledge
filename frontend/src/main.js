15b372aed713ec3d4e1e5cc3778440d79cc59267
marcelorocfer <marceloengecomp@gmail.com>
2020-08-25 15:49:13 -0300

    Remoção do token temporário do arquivo main  Mudanças a serem submetidas: 	modified:   frontend/src/main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // Temporário
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hcmNlbG8gUi4gRmVycmVpcmEiLCJlbWFpbCI6Im1hcmNlbG9AbWFyY2Vsby5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk4MzU4NzM1LCJleHAiOjE1OTg2MTc5MzV9.mIKX-qoda_qVOmTJVniQMCMrKE2Y3kG6eig6AFzXEHE'
+require('axios').defaults.headers.common['Authorization'] = 'bearer Aqui vai o token'
 
 new Vue({
   store,
